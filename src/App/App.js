c64a9964d9293a74efc27cad743e397f79d397c1
enowqi <anjyu20010227@gmail.com>
2021-11-14 12:40:13 +0000

    クイズを埋め込める用に変更した

--- src/App/App.js
+++ src/App/App.js
@@ -1,40 +1,43 @@
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Table, TableCaption, Tr, Th, Td, Thead, Tbody } from '@chakra-ui/table'
+import Quiz0 from '../Quiz/quiz000';
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        <Table variant="simple">
+        <Table size="sm">
           <TableCaption>プログラミング基礎100本ノック</TableCaption>
           <Thead>
             <Tr>
               <Th>番号</Th>
-              <Th>問題</Th>
-              <Th>答え</Th>
+              <Th>内容</Th>
             </Tr>
           </Thead>
           <Tbody>
             <Tr>
               <Td>000</Td>
-              <Td>実行するとHello World!と表示するプログラムを作成せよ。</Td>
               <Td>
-                {/* ここにクイズ000をおく */}
+                <Quiz0 />
               </Td>
             </Tr>
             <Tr>
               <Td>001</Td>
-              <Td>12345+23456を計算して結果を表示するプログラムを作成せよ。</Td>
               <Td>
                 {/* ここにクイズ001をおく */}
               </Td>
             </Tr>
             <Tr>
               <Td>002</Td>
-              <Td>12345を7で割った余りを表示するプログラムを作成せよ。</Td>
               <Td>
                 {/* ここにクイズ002をおく */}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>003</Td>
+              <Td>
+                {/* ここにクイズ003をおく */}
               </Td>
             </Tr>
           </Tbody>
--- src/Quiz/quiz000.js
+++ src/Quiz/quiz000.js
@@ -1,1 +1,11 @@
 console.log("hello world!")
+
+function Quiz0() {
+    return (
+        <>
+            <h1>こんにちは</h1>
+        </>
+    )
+};
+
+export default Quiz0;
