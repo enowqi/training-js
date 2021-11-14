202c83476201e01aecd96265642a721d212fcc0f
enowqi <anjyu20010227@gmail.com>
2021-11-14 12:26:43 +0000

    クイズ用に事前準備

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,44 @@
 import './App.css';
-import { Progress } from '@chakra-ui/progress';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Table, TableCaption, Tr, Th, Td, Thead, Tbody } from '@chakra-ui/table'
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        <Progress value={50} />
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Table variant="simple">
+          <TableCaption>プログラミング基礎100本ノック</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>番号</Th>
+              <Th>問題</Th>
+              <Th>答え</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>000</Td>
+              <Td>実行するとHello World!と表示するプログラムを作成せよ。</Td>
+              <Td>
+                {/* ここにクイズ000をおく */}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>001</Td>
+              <Td>12345+23456を計算して結果を表示するプログラムを作成せよ。</Td>
+              <Td>
+                {/* ここにクイズ001をおく */}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>002</Td>
+              <Td>12345を7で割った余りを表示するプログラムを作成せよ。</Td>
+              <Td>
+                {/* ここにクイズ002をおく */}
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
       </div>
     </ChakraProvider>
   );
