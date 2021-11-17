e90f52b8ddeffa1b7eb1c10209c85753e622c0ed
enowqi <anjyu20010227@gmail.com>
2021-11-17 08:31:13 +0000

    足し算

--- src/Quiz/quiz001.js
+++ src/Quiz/quiz001.js
@@ -1,9 +1,11 @@
 console.log(12345+23456)
-
+function 足し算(x,y){
+    return x+y
+}
 function Quiz1() {
     return (
         <>
-            <h1>こんにちは</h1>
+            <h1>{足し算(9,8)}</h1>
         </>
     )
 };
