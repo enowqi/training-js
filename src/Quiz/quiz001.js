3678f4895f99a984f4be677ee8cb14694dc43584
enowqi <anjyu20010227@gmail.com>
2021-11-17 08:49:28 +0000

    文字を大きくした

--- src/Quiz/quiz001.js
+++ src/Quiz/quiz001.js
@@ -1,3 +1,5 @@
+import { Text } from "@chakra-ui/layout";
+
 console.log(12345+23456)
 function 足し算(x,y){
     return x+y
@@ -5,7 +7,7 @@ function 足し算(x,y){
 function Quiz1() {
     return (
         <>
-            <h1>{足し算(12345,23456)}</h1>
+            <Text fontSize="5xl">{足し算(12345,23456)}</Text>
         </>
     )
 };
